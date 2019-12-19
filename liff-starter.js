48c955f88c02ff7f0df4ab520c4d837b749925fa
djbuzro <djbuzro@gmail.com>
2019-12-20 00:03:30 +0700

    Create liff-starter.js

---
+++ liff-starter.js
@@ -0,0 +1,18 @@
+/**
+* Initialize LIFF
+* @param {string} myLiffId The LIFF ID of the selected element
+*/
+function initializeLiff(myLiffId) {
+    liff
+        .init({
+            liffId: '1653655592-0nmZoA5p';
+        })
+        .then(() => {
+            // start to use LIFF's api
+            initializeApp();
+        })
+        .catch((err) => {
+            document.getElementById("liffAppContent").classList.add('hidden');
+            document.getElementById("liffInitErrorMessage").classList.remove('hidden');
+        });
+}
